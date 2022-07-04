e5192a07bae241bfeb7d78771cc94f506856c20a
Tarek Azzazi <azzazi.tarek@gmail.com>
2022-07-04 16:08:51 -0500

    meal form wireframe added

--- src/components/MealPlan/MealPlan.jsx
+++ src/components/MealPlan/MealPlan.jsx
@@ -143,6 +143,38 @@ function MealPlan() {
         </table>
       </div>
       {/* ////////////////////////END OF MEAL TABLE?//////////////////////////// */}
+
+      {/* ////////////////Add A New Meal Form////////////////////////////////// */}
+      <div>
+        <h5>Add a new meal...</h5>
+        <form>
+          <label htmlFor="name">Meal Name:</label>
+          <input type="text" />
+
+          <label htmlFor="type">Meal Type:</label>
+          <select name="type" id="mealtype">
+            <option value="Breakfest">Breakfest</option>
+            <option value="Lunch">Lunch</option>
+            <option value="Dinner">Dinner</option>
+          </select>
+
+          <label htmlFor="date"> Day:</label>
+          <select name="date" id="date">
+            <option value="Monday">Monday</option>
+            <option value="Tuesday">Tuesday</option>
+            <option value="Wednesday">Wednesday</option>
+            <option value="Thursday">Thursday</option>
+            <option value="Friday">Friday</option>
+            <option value="Saturday">Saturday</option>
+            <option value="Sunday">Sunday</option>
+          </select>
+
+          <label htmlFor="total"> meal total carbs:</label>
+          <input id="total" type="number" min="0"  />
+
+          <button>Submit</button>
+        </form>
+      </div>
     </div>
   );
 }
