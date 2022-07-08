10aa410f2545b9eae3bcea3ce54ca1c25f533a02
Tarek Azzazi <azzazi.tarek@gmail.com>
2022-07-08 09:26:25 -0500

    set inputs to state variables

--- src/components/HealthProfilePage/HealthProfilePage.jsx
+++ src/components/HealthProfilePage/HealthProfilePage.jsx
@@ -1,8 +1,20 @@
 import React from "react";
 import "./HealthProfilePage.css";
+import { useState, useEffect } from "react";
+
 function UserProfilePage() {
+  const [name, setNickName] = useState("");
+  const [wieght, setWieght] = useState([]);
+  const [carbLimit, setWeeklyCarbLimit] = useState([]);
+  const [doctorNotes, setDoctorNotes] = useState("");
+
   function sumbitHealthForm(evt) {
     evt.preventDefault();
+    console.log("made it into submit form");
+    console.log(name);
+    console.log(wieght);
+    console.log(carbLimit);
+    console.log(doctorNotes);
   }
   return (
     <div className="container">
@@ -10,20 +22,47 @@ function UserProfilePage() {
 
       <form onSubmit={sumbitHealthForm}>
         <label htmlFor="text">Name:</label>
-        <input type="text" />
+        <input
+          required
+          type="text"
+          onChange={(e) => {
+            setNickName(e.target.value);
+          }}
+        />
         <br />
         <label htmlFor="number">Weekly Carb limit</label>
-        <input type="number" />
+        <input
+          required
+          type="number"
+          onChange={(e) => {
+            setWeeklyCarbLimit(e.target.value);
+          }}
+        />
 
         <br />
         <label htmlFor="number">Weight:</label>
-        <input type="number" min={0} max={300} />
+        <input
+          required
+          type="number"
+          min={0}
+          max={300}
+          onChange={(e) => {
+            setWieght(e.target.value);
+          }}
+        />
         <label htmlFor="number">lbs</label>
         <br />
         <label htmlFor="comment">Doctor Notes</label>
         <br />
-        <textarea name="comment"></textarea>
-        <button>Submit</button>
+        <textarea
+          name="comment"
+          onChange={(e) => {
+            setDoctorNotes(e.target.value);
+          }}
+        />
+        <div>
+          <button>Submit</button>
+        </div>
       </form>
     </div>
   );
