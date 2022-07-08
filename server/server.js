771e2d1874b4c910b1d1d948bf3819160224ff9a
Tarek Azzazi <azzazi.tarek@gmail.com>
2022-07-08 15:36:13 -0500

    removed a router bug in servers.js

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const passport = require("./strategies/user.strategy");
 // Route includes
 const userRouter = require("./routes/user.router");
 const mealRouter = require("./routes/meal.router");
-const healthProfileRouter = require("./routes/healthprofile.router");
+
 // Body parser middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -25,7 +25,6 @@ app.use(passport.session());
 /* Routes */
 app.use("/api/user", userRouter);
 app.use("/api/meals", mealRouter);
-app.use("/api/userHealthInfo", healthProfileRouter);
 
 // Serve static files
 app.use(express.static("build"));
