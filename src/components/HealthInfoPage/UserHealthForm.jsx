ad9ab7d39c7ff18fa1a05942812c5f306cf42fb6
Tarek Azzazi <azzazi.tarek@gmail.com>
2022-07-11 11:14:20 -0500

    Edit Health inputs shown

--- src/components/HealthInfoPage/UserHealthForm.jsx
+++ src/components/HealthInfoPage/UserHealthForm.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
-
+import "./Styles/HealthInfoPage.css";
 function HealthInfoPage() {
   const user = useSelector((store) => store.user);
 
@@ -13,24 +12,49 @@ function HealthInfoPage() {
     <div className="container">
       <h2>My Health Info</h2>
       <ul>
-        <li>Welcome, {user.name}</li>
+        <li>
+          Welcome,{" "}
+          <input
+            type="text"
+            required="required"
+            placeholder="Enter a new nickName"
+            value={user.name}
+          />
+        </li>
 
-        <li> You currently weigh: {user.wieght} lbs </li>
+        <li>
+          You currently weigh:{" "}
+          <input
+            type="number"
+            required="required"
+            placeholder="Enter your weight"
+            value={user.wieght}
+          />
+          lbs
+        </li>
 
         <li>
           Carb Limtit:{" "}
           <input
             type="number"
             required="required"
-            placeholder="Enter your carb limit"
+            placeholder="Enter your weekly carb limit"
+            value={user.max_carbs}
           />
         </li>
 
-        <li>{user.doctor_notes}</li>
+        <li>
+          <textarea
+            type="text"
+            required="required"
+            placeholder="any new notes"
+            value={user.doctor_notes}
+          />
+        </li>
       </ul>
 
       <div>
-        <button onClick={EditHealthInfo}> Edit </button>
+        <button onClick={EditHealthInfo}> Save </button>
       </div>
     </div>
   );
