0bd659d25474586a46ea6f085466a43a09d3a022
Tarek Azzazi <azzazi.tarek@gmail.com>
2022-08-22 15:24:10 -0500

    final commit

--- database.sql
+++ database.sql
@@ -23,3 +23,18 @@ CREATE TABLE "meals"(
 "notes" VARCHAR(255)
 );
 
+-- Dummy Data
+
+INSERT INTO meals (user_id, date, meal_name, meal_type, carbs,blood_sugar_lvl)
+ VALUES 
+ (1,'2022/08/24','Apple pie', 'Breakfast', 120, 125), 
+ (1,'2022/08/24','sandwich', 'Lunch', 125, 135),
+ (1,'2022/08/24','pizza', 'Dinner', 130, 125),
+ (1,'2022/08/25','Cearl', 'Breakfast', 120, 100), 
+ (1,'2022/08/25','Soup', 'Lunch', 110, 140),
+ (1,'2022/08/25','pizza', 'Dinner', 125, 130),
+ (1,'2022/08/26','Eggs', 'Breakfast', 120, 100), 
+ (1,'2022/08/26','Grilled Cheese', 'Lunch', 110, null),
+ (1,'2022/08/26','Cheesburger', 'Dinner', 125, null
+ );
+  
